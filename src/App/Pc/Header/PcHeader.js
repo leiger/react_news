8ad4260af2f27311dba944afd0f144d54973177e
leiger <leiger@bupt.edu.cn>
2018-05-22 12:01:52 -0400

    header modify

--- src/App/Pc/Header/PcHeader.js
+++ src/App/Pc/Header/PcHeader.js
@@ -1,26 +1,52 @@
 import React, {Component} from 'react';
-import {Row, Col, Menu, Icon} from 'antd';
+import {Row, Col, Menu, Icon, Form, Tabs, Modal, message, Input, Button, Checkbox} from 'antd';
 import Logo from '../../images/logo.png';
 import './PcHeader.css';
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
 
 class PcHeader extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      section: 'World'
+      section: 'World',
+      modalVisible: false,
+      action: 'login',
+      hasLogined: false,
+      userNickName: 'none',
+      userId: 0,
     };
   }
+
   setSection = (e) => {
     this.setState({
       section: e.key
     })
   };
 
+  setModalVisible(val) {
+    this.setState({
+      modalVisible: val
+    })
+  }
+
   render() {
+    let {getFieldDecorator} = this.props.form;
+    let userShow = this.state.hasLogined
+      ?
+      <span>
+        <Button>{this.state.userNickName}</Button>
+        <Button type="ghoast">logout</Button>
+      </span>
+      :
+      <Button onClick={() => this.setModalVisible(true)}>
+        register/login
+      </Button>
+    ;
+
     return (
       <header>
         <Row>
@@ -38,9 +64,6 @@ class PcHeader extends Component {
               <Menu.Item key="Local">
                 <Icon type="appstore"/>Local
               </Menu.Item>
-              <Menu.Item key="Business">
-                <Icon type="appstore"/>Business
-              </Menu.Item>
               <Menu.Item key="Technology">
                 <Icon type="appstore"/>Technology
               </Menu.Item>
@@ -53,13 +76,27 @@ class PcHeader extends Component {
               <Menu.Item key="Science">
                 <Icon type="appstore"/>Science
               </Menu.Item>
+              {userShow}
             </Menu>
           </Col>
-          <Col span={2}></Col>
         </Row>
+
+        {/*modal*/}
+        <Modal title="register/login" wrapClassName="vertical-center-modal" visible={this.state.modalVisible}
+               onOk={() => this.setModalVisible(false)} onCancel={() => this.setModalVisible(false)}>
+          <Tabs defaultActiveKey="login">
+            <TabPane tab="login" key="login">
+              tabpane1
+            </TabPane>
+            <TabPane tab="register" key="register">
+              tabpane2
+            </TabPane>
+          </Tabs>
+        </Modal>
+
       </header>
     )
   }
 }
 
-export default PcHeader;
+export default PcHeader = Form.create()(PcHeader);
